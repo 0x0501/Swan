33ff5bfed117e217a3cd84f40aad1a907ebe9158
Rayn <Rayn@example.com>
2024-11-23 22:21:42 +0800

    fix: add style restriction when qt application started

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def exception_hook(exc_type, exc_value, exc_traceback):
 def main():
     # 设置全局异常处理
     sys.excepthook = exception_hook
+    
+    # Windows 10/11 风格设置
+    QApplication.setStyle("Macintosh")
 
     app = QApplication(sys.argv)
     # 设置Qt的异常处理
